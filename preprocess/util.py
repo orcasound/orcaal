6cc23feab9fe761e4444ac92e7a752a7e198ee78
wetdog <jose091@gmail.com>
2021-08-06 00:36:56 +0200

    add util functions to extract and save embeddings

--- preprocess/util.py
+++ preprocess/util.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 from skimage.restoration import denoise_wavelet
 import os
 
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_io as tfio
+
 
 def apply_per_channel_energy_norm(spectrogram):
     """Apply PCEN.
@@ -88,6 +93,8 @@ def spec_plot_and_save(denoised_data, f_name, output_dir):
     plt.close(fig)
 
 
+
+
 def select_spec_case(plot_path, folder_path, pcen=False, wavelet=False):
     """Selects the preprocessing steps to be applied to the spectrogram.
 
@@ -116,6 +123,86 @@ def select_spec_case(plot_path, folder_path, pcen=False, wavelet=False):
         f_name = os.path.basename(file)
 
         spectrogram_data = librosa.feature.melspectrogram(data, sr=sr, power=1)
+        if pcen and not wavelet:
+            pcen_spec = apply_per_channel_energy_norm(spectrogram_data)
+        elif pcen and wavelet:
+            pcen_spec = apply_per_channel_energy_norm(spectrogram_data)
+            spectrogram_data = wavelet_denoising(pcen_spec)
+        spec_plot_and_save(spectrogram_data, f_name, plot_path)
+
+
+def extract_embedding_from_signal(waveform,model):
+    "Assumes the waveform has the correct sample rate"
+
+    if model == 'vggish':
+        embedding = vggish_model(waveform)
+    if model == 'yamnet':
+        scores, embedding, log_mel_spectrogram = yamnet_model(waveform)
+    if model == 'humpback':
+        
+        waveform = tf.Variable(waveform.reshape([-1,1]),dtype=tf.float32)
+        waveform = tf.expand_dims(waveform, 0)  # makes a batch of size 1
+        pcen_spectrogram = humpback_model.front_end(waveform)
+        
+        # zero pad if lenght not a multiple of 128
+        w_size = 128 # 3.84 seconds context window
+        
+        if pcen_spectrogram.shape[1] % w_size != 0:
+            even_n = w_size - pcen_spectrogram.shape[1] % w_size
+            pcen_spectrogram = tf.concat([pcen_spectrogram,tf.zeros([1,even_n,64])], axis=1)
+
+        n_frames = int(pcen_spectrogram.shape[1]/w_size)
+
+        batch_pcen_spectrogram = tf.reshape(pcen_spectrogram,shape=(n_frames,w_size,64)) 
+        embedding = humpback_model.features(batch_pcen_spectrogram)
+    
+    return embedding
+
+def save_embedding(mean_embedding, f_name, output_dir):
+    """" Save embedding as tsv file """
+    embedding_file = os.path.join(output_dir, f"{f_name[:-4]}.tsv")
+    with open(embedding_file, 'a') as f:
+        for val in mean_embedding:
+            f.write('{0:.4f}\t'.format(val))
+
+
+def select_spec_case_embedding(plot_path, folder_path, pcen=False, wavelet=False,model='yamnet'):
+    """Selects the preprocessing steps to be applied to the spectrogram.
+
+    Depending upon the choices entered by the user this function would
+    select the necessary preprocessing stages and call their respective
+    functions.
+
+    Args:
+        plot_path: The output path where we want to plot the spectrograms.
+        folder: The input_path which contains the audio that would
+            be used to generate spectrograms.
+        pcen: Could be set to True if we want to apply PCEN to spectrograms.
+        wavelet:Could be set to true if we want to apply Wavelet denoising
+            to the spectrograms.
+        model: The model to use to extract the embeddings, options are 'vggish',
+        'yamnet', and 'humpback'
+
+    Returns:
+        None.
+    """
+    onlyfiles = [
+        f for f in os.listdir(folder_path)
+        if os.path.isfile(os.path.join(folder_path, f))
+    ]
+    for id, file in enumerate(onlyfiles):
+        fpath = os.path.join(folder_path, file)
+        data, sr = librosa.core.load(fpath, res_type="kaiser_best")
+        f_name = os.path.basename(file)
+
+        spectrogram_data = librosa.feature.melspectrogram(data, sr=sr, power=1)
+        # compute embedding
+        embedding = extract_embedding_from_signal(data,model)
+        # aggregate data
+        mean_embedding = tf.reduce_mean(embedding,axis=0)
+        # save embedding tsv
+        save_embedding(mean_embedding,f_name,plot_path)
+
         if pcen and not wavelet:
             pcen_spec = apply_per_channel_energy_norm(spectrogram_data)
         elif pcen and wavelet:
