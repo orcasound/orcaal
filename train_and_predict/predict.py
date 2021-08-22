410fad82778d39a9ae4737c36879caa0b262d5e5
wetdog <jose091@gmail.com>
2021-08-22 19:38:07 +0200

    fix paths and tqdm progress bar

--- train_and_predict/predict.py
+++ train_and_predict/predict.py
@@ -3,6 +3,8 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import subprocess
 from datetime import datetime
 import os
+from tqdm import tqdm
+import pandas as pd
 
 locations = {'orcasoundlab': 'Haro Strait'}
 
@@ -22,6 +24,10 @@ def get_predictions_on_unlabeled(s3_model_path, s3_unlabeled_path, img_width,
         'aws', 's3', 'sync', f'{s3_unlabeled_path}spectrograms/',
         local_unlabeled_path, '--delete'
     ])
+    ## Download and read csv files with embeddings
+    
+    
+    
 
     image_generator = ImageDataGenerator(rescale=1. / 255)
     data_generator = image_generator.flow_from_directory(
@@ -56,12 +62,11 @@ def get_predictions_on_unlabeled(s3_model_path, s3_unlabeled_path, img_width,
 def get_predictions_local(local_model_path, unlabeled_path, img_width,
                         img_height):
     model = load_model(local_model_path)
-    local_unlabeled_path = f'{unlabeled_path}spectrograms/'
     image_generator = ImageDataGenerator(rescale=1. / 255)
     data_generator = image_generator.flow_from_directory(
-        local_unlabeled_path,
-        # only read images from `unlabeled` directory
-        classes=[local_unlabeled_path],
+        unlabeled_path,
+        # only read images from `spectrograms` directory
+        classes=['spectrograms'],
         # don't generate labels
         class_mode=None,
         # don't shuffle
@@ -69,15 +74,20 @@ def get_predictions_local(local_model_path, unlabeled_path, img_width,
         # use same size as in training
         target_size=(img_width, img_height),
         batch_size=64)
-
     predictions = model.predict(data_generator).tolist()
 
+    # read csv file with embeddings 
+    embeddings_file = os.path.join(unlabeled_path, os.path.basename(unlabeled_path) + '_embeddings.csv')
+    df = pd.read_csv(embeddings_file)
+    df.set_index('filename',inplace=True)
+
     predictions_list = []
-    for i in range(len(predictions)):
+    for i in tqdm(range(len(predictions))):
         cur_prediction = {}
         cur_prediction['predicted_value'] = predictions[i][0]
         cur_file = os.path.split(data_generator.filenames[i])[1].split('.')[0]
-        cur_prediction['audio_path'] = f'{local_unlabeled_path}/mp3/{cur_file}.mp3'
+        cur_prediction['audio_path'] = f'{unlabeled_path}/mp3/{cur_file}.mp3'
+        cur_prediction['representativeness'] = df.loc[cur_file+'.tsv']['representativeness']
         location, timestamp = cur_file.split('_')
         cur_prediction['location'] = locations[location]
         cur_prediction['duration'] = 3
