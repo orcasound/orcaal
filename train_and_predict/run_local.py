a92b47f48df9376abf5c1f0ea5c7c0c1c3225dcb
wetdog <jose091@gmail.com>
2021-08-22 19:41:34 +0200

    run local as function

--- train_and_predict/run_local.py
+++ train_and_predict/run_local.py
@@ -1,11 +1,30 @@
 from predict import get_predictions_local
+import pandas as pd
+import os
+import argparse
 
 
-model_path = "/home/wetdog/orcaal/train_and_predict/models/srkw_5.h5"
-unlabeled_path = "/home/wetdog/orcaal/preprocess/unlabeled_test/"
-img_width = 607
-img_height = 617
+def main(unlabeled_path):
+    """" Get predictions using local preprocessed files to save the
+    data that is passed to the predict API endpoint"""
 
-predictions = get_predictions_local(model_path, unlabeled_path, img_width, img_height)
+    model_path = "/home/wetdog/orcaal/train_and_predict/models/srkw_5.h5"
+    img_width = 607
+    img_height = 617
+    predictions = get_predictions_local(model_path, unlabeled_path, img_width, img_height)
+    df = pd.DataFrame(predictions)
+    df.to_csv(os.path.join(unlabeled_path,'metadata.tsv'), sep='\t',index=False)
 
-print(predictions)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument(
+        'unlabeled_path',
+        help=
+        'path of the directory that contains preprocessed data created after preprocess_unlabeled run'
+    )
+
+    args = parser.parse_args()
+    main(args.unlabeled_path)
