bf956e20ee1658d4c8dd4496c0d0ae70a8781e68
Benjamintdk <bc.chew.hong.ee@gmail.com>
2022-05-05 09:20:22 +0800

    initial commit for prod compose file

--- api/boot.sh
+++ api/boot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pipenv run flask db upgrade
-exec pipenv run gunicorn -b :5000 --access-logfile - --error-logfile - app:app
+flask db upgrade
+gunicorn -b :5000 --access-logfile - --error-logfile - app:app
