3c6a83d138d90237e0acdd66363aafa4f2b16217
Benjamin Chew <45474952+Benjamintdk@users.noreply.github.com>
2022-05-14 18:29:00 +0800

    Update train.py
    Reverting previous changes

--- train_and_predict/train.py
+++ train_and_predict/train.py
@@ -15,10 +15,8 @@ def train(s3_model_path, s3_labeled_path, img_width, img_height, epochs):
     new_model_name = '_'.join(
         local_model_path.split('_')[:-1] + [new_model_version])
     new_model_name = f'{new_model_name}.h5'
-    subprocess.run(['aws', 's3', 'ls', 's3://acoustic-sandbox/orcaal-dev/', '--region', 'eu-west-1'])
     if not os.path.isfile(local_model_path):
-        subprocess.run(['aws', 's3', 'cp', s3_model_path, '.', '--region', 'eu-west-1'])
-    subprocess.run(['ls'])
+        subprocess.run(['aws', 's3', 'cp', s3_model_path, '.'])
     model = load_model(local_model_path)
     # Download data from s3 to `labeled` directory
     subprocess.run(['aws', 's3', 'sync', s3_labeled_path, local_labeled_path])
