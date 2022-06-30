00d7ee08e4bf36a2a0fee61cf900122a78f52976
Benjamintdk <bc.chew.hong.ee@gmail.com>
2022-06-30 16:08:39 +0800

    created config files for both services using datahandlers

---
+++ api/app/config.py
@@ -0,0 +1,22 @@
+import os
+
+from utils.AWSDataHandler import AWSDataHandler
+from utils.IDataHandler import IDataHandler
+from utils.LocalDataHandler import LocalDataHandler
+
+
+class DataHandlerConfig:
+    def get_datahandler(self, handler_type: str) -> IDataHandler:
+        try:
+            handler_type = handler_type.lower()
+        except AttributeError as e:
+            print(e.output)
+        if handler_type == "AWS":
+            return AWSDataHandler
+        elif handler_type == "local":
+            return LocalDataHandler
+        else:
+            raise ValueError(f"Handler type {handler_type} input is invalid")
+
+
+data_handler = DataHandlerConfig(os.environ.get("DATA_HANDLER"))
